d31eb7b84d9b0221b8bc9866f9ab922eb8f4568a
geekymoose <constantin.masson@umontreal.ca>
2020-03-22 19:27:32 +0100

    Add method to concat strings

---
+++ src/engine/str.c
@@ -0,0 +1,30 @@
+#include "str.h"
+
+#include "engine/assertions.h"
+#include "engine/log.h"
+
+#include <string.h>
+
+void concatStrings(char* _dest, size_t _destBufferSize, const char* _str1, const char* _str2)
+{
+    ASSERT_MSG(_dest != NULL, "Invalid parameter: destination string should not be null");
+    ASSERT_MSG(_str1 != NULL, "Invalid parameter: source string should not be null");
+    ASSERT_MSG(_str2 != NULL, "Invalid parameter: source string should not be null");
+    ASSERT_MSG(_destBufferSize > 0, "Invalid parameter: destination string size is not valid");
+
+    if (_dest == NULL || _str1 == NULL || _str2 == NULL) {
+        LOG_ERR("Unable to concat string (NULL given)");
+        return;
+    } else if (_destBufferSize <= 0) {
+        LOG_ERR("Unable to concat string (invalid size given)");
+        return;
+    }
+
+    memset(_dest, 0, _destBufferSize);
+    strncpy(_dest, _str1, _destBufferSize - 1); // Size includes '\0'
+
+    int sizeRemains = _destBufferSize - strlen(_dest) - 1; // -1 to account the '\0'
+    if (sizeRemains > 0) {
+        strncat(_dest, _str2, sizeRemains);
+    }
+}
---
+++ src/engine/str.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <stdlib.h>
+
+void concatStrings(char* _dest, size_t _destBufferSize, const char* _str1, const char* _str2);
